bfc68379e4df7e6cb56c8e905da10b4682cfa353
Sajjad Ziaenezhad <shirazy.sajjad@gmail.com>
2026-05-10 13:02:46 +0330

    feat: rtl

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,23 +4,37 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: 'دانشنامه کارگاه فنّاوری دِراک',
+			customCss: ["./src/styles/wiki-rtl.css"],
+      social: [
+        {
+          icon: 'gitlab',
+          label: 'Chabokan Gitlab',
+          href: 'https://gitlab.chabokan.net/derock.ir/wiki',
+        },
+      ],
+      defaultLocale: 'root',
+      locales: {
+        root: {
+          label: 'فارسی',
+          lang: 'fa-IR',
+        },
+      },
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Example Guide', slug: 'guides/example' },
+          ],
+        },
+        {
+          label: 'Reference',
+          items: [{ autogenerate: { directory: 'reference' } }],
+        },
+      ],
+    }),
+  ],
 });
